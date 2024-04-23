394fd5355e8bf545c16aa930035063915b84a51e
이병수 <lbsoo1021@naver.com>
2024-04-23 22:53:45 +0900

    backend 시작

---
+++ backend/src/main/java/com/ssafy/weSync/global/domain/BaseTime.java
@@ -0,0 +1,26 @@
+package com.ssafy.weSync.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseTime {
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_by")
+    private LocalDateTime deletedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "deleted_by")
+    private Expunger deletedBy;
+}
