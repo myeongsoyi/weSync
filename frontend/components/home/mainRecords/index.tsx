ecb22b552fa71bc49fcb131a23fe742a6d9d91dc
황인규 <rlawldbs42@gmail.com>
2024-04-26 16:13:34 +0900

    feat: 메인페이지 내 녹음 요약 컴포넌트 틀 추가

--- frontend/components/home/mainRecords/index.tsx
+++ frontend/components/home/mainRecords/index.tsx
@@ -1,7 +1,30 @@
-export default function MainRecords() {
+import { getMainRecords } from '@/services/home';
+import ListRecord from './listRecord';
+import styles from './index.module.scss';
+
+
+export default async function MainTeams() {
+  const records = await getMainRecords();
+
+  // const teams: {
+  //     id: number;
+  //     name: string;
+  //     song: string;
+  //     myPosition: {
+  //         position: string;
+  //         color: string;
+  //     };
+  //     teamImg: string;
+  //     members: {
+  //         id: number;
+  //         name: string;
+  //         profileImg: string;
+  //     }[];
+  // }[]
+
   return (
-    <>
-      <h1>RECORDS</h1>
-    </>
+    <div className={styles.outer}>
+      <ListRecord records={records} />
+    </div>
   );
 }
