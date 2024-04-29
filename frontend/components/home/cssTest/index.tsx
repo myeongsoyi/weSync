3b6308422e9ca670d71dcea6e8b7c31f5a16cc43
황인규 <rlawldbs42@gmail.com>
2024-04-29 09:54:59 +0900

    init: zustand 설치

--- frontend/components/home/cssTest/index.tsx
+++ frontend/components/home/cssTest/index.tsx
@@ -1,4 +1,6 @@
 import styles from './index.module.scss';
+import FirstChild from '@/components/common/storeTest/FirstChild';
+import SecondChild from '@/components/common/storeTest/SecondChild';
 
 export default function cssTest() {
   return (
@@ -14,6 +16,8 @@ export default function cssTest() {
       <p>css 테스트</p>
       <hr />
       <span>Content : 스크롤이 있는 경우</span>
+      <FirstChild />
+      <SecondChild />
     </div>
   );
 }
