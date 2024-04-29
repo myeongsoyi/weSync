af8d3d5f245b9fb0f22053b8c3af96f4d2300afb
김평섭 <k1996914@naver.com>
2024-04-29 13:44:56 +0900

    feat: Ref user, token

--- backend/src/main/java/com/ssafy/weSync/token/controller/TokenController.java
+++ backend/src/main/java/com/ssafy/weSync/token/controller/TokenController.java
@@ -1,24 +1,23 @@
 package com.ssafy.weSync.token.controller;
 
 import com.ssafy.weSync.global.ApiResponse.Response;
+import com.ssafy.weSync.token.dto.TokenDto;
 import com.ssafy.weSync.token.service.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")//추후 프론트 배포 주소로 변경
 public class TokenController {
 
     @Autowired
     private TokenService tokenService;
 
     @PostMapping("access-token/issue")
-    public ResponseEntity<Response> updateAccessToken(HttpServletRequest request) throws JSONException {
-        ResponseEntity<Response> response = tokenService.updateAccessToken(request);
-        return response;
+    public ResponseEntity<Response<TokenDto>> updateAccessToken(HttpServletRequest request){
+        return tokenService.updateAccessToken(request);
     }
 }
