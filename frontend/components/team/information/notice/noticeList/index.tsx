f160ea9f69d098c50a574fdbc2316020f8c5a971
yeon <lsy9752@naver.com>
2024-04-29 18:02:53 +0900

    init: teampage info

---
+++ frontend/components/team/information/notice/noticeList/index.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Avatar, Button, List, Skeleton } from 'antd';
+import Item from 'antd/es/list/Item';
+import { Meta } from 'antd/es/list/Item';
+
+interface IParams {
+  notices: {
+    id: number;
+    date: string;
+    detail: string;
+    pinned: boolean;
+  }[];
+}
+
+
+export default function NoticeList({ notices }: IParams) {
+  console.log(notices);
+
+
+  return (
+    <div className='h-96 overflow-auto px-5 py-2'>
+      <List
+        className="demo-loadmore-list"
+        // loading={initLoading}
+        itemLayout="horizontal"
+        dataSource={notices}
+        renderItem={(item) => (
+          <Item>
+            {/* <Skeleton avatar title={false} loading={item.} active> */}
+            <Meta className=' text-center'
+              title={<p>{item.detail}</p>}
+              description={<p>{item.date}</p>}
+            />
+            {item.pinned ? <div>pinned</div> : <div>space</div> }
+            {/* </Skeleton> */}
+          </Item>
+        )}
+      />
+    </div>
+  );
+}
