34aadee1486db3b3ffd9fd49f469a15bbbfead80
황인규 <rlawldbs42@gmail.com>
2024-05-01 20:23:56 +0900

    feat: private 탭 틀 추가

--- frontend/components/team/workspace/private/index.tsx
+++ frontend/components/team/workspace/private/index.tsx
@@ -1,11 +1,15 @@
 import styles from "./index.module.scss";
+import Table from "./table";
+import { getTeamPrivateRecords } from '@/services/team';
 
-export default function TeamWorkspacePrivate() {
+export default async function TeamWorkspacePrivate() {
+  const records = await getTeamPrivateRecords();
+
   return (
     <div className={styles.article}>
       <h1 className={styles.title}>My Records</h1>
       <div className={styles.table}>
-        asd
+        <Table records={records}/>
       </div>
     </div>
   );
