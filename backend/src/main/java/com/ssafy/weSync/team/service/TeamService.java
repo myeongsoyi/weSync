e30080bbe6ad6b131b206f33e84befcb1e9034b9
김평섭 <k1996914@naver.com>
2024-05-08 17:22:40 +0900

    feat: Add position

--- backend/src/main/java/com/ssafy/weSync/team/service/TeamService.java
+++ backend/src/main/java/com/ssafy/weSync/team/service/TeamService.java
@@ -4,14 +4,12 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.ssafy.weSync.global.ApiResponse.ErrorResponse;
 import com.ssafy.weSync.global.ApiResponse.Response;
 import com.ssafy.weSync.team.dto.request.CreateTeamInfoDto;
+import com.ssafy.weSync.team.dto.request.CustomPositionDto;
 import com.ssafy.weSync.team.dto.request.EditTeamInfoDto;
+import com.ssafy.weSync.team.dto.request.ScorePositionDto;
 import com.ssafy.weSync.team.dto.response.*;
-import com.ssafy.weSync.team.entity.Invitation;
-import com.ssafy.weSync.team.entity.Team;
-import com.ssafy.weSync.team.entity.TeamUser;
-import com.ssafy.weSync.team.repository.InvitationRepository;
-import com.ssafy.weSync.team.repository.TeamRepository;
-import com.ssafy.weSync.team.repository.TeamUserRepository;
+import com.ssafy.weSync.team.entity.*;
+import com.ssafy.weSync.team.repository.*;
 import com.ssafy.weSync.s3.service.S3Service;
 import com.ssafy.weSync.user.entity.User;
 import com.ssafy.weSync.user.repository.UserRepository;
@@ -19,6 +17,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,15 +35,27 @@ public class TeamService {
     private final AmazonS3Client amazonS3Client;
     private  final UserRepository userRepository;
 
+    private final ColorRepository colorRepository;
+
+    private final PositionRepository positionRepository;
+
+    private final ScoreRepository scoreRepository;
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public TeamService(TeamRepository teamRepository, InvitationRepository invitationRepository, TeamUserRepository teamUserRepository, AmazonS3Client amazonS3Client, UserRepository userRepository) {
+    private static final String[] DefaultPositionName = {"소프라노","메조소프라노", "알토", "바리톤", "테너", "베이스", "퍼커션"};
+    private static final Long[] DefaultColorId = {1L,1L,1L,1L,1L,1L,1L}; // 디폴트 포지션별 colorId
+
+    public TeamService(TeamRepository teamRepository, InvitationRepository invitationRepository, TeamUserRepository teamUserRepository, AmazonS3Client amazonS3Client, UserRepository userRepository, ColorRepository colorRepository, PositionRepository positionRepository, ScoreRepository scoreRepository) {
         this.teamRepository = teamRepository;
         this.invitationRepository = invitationRepository;
         this.teamUserRepository = teamUserRepository;
         this.amazonS3Client = amazonS3Client;
         this.userRepository = userRepository;
+        this.colorRepository = colorRepository;
+        this.positionRepository = positionRepository;
+        this.scoreRepository = scoreRepository;
     }
 
     //팀 생성
@@ -166,6 +178,7 @@ public class TeamService {
             if(!team.getIsFinished()){
                 ShortActiveTeamInfoDto shortActiveTeamInfoDto = new ShortActiveTeamInfoDto();
                 shortActiveTeamInfoDto.setTeamId(team.getTeamId());
+                shortActiveTeamInfoDto.setTeamName(team.getTeamName());
                 if(team.getSongName()!=null){
                     shortActiveTeamInfoDto.setSongNameExist(true);
                     shortActiveTeamInfoDto.setSongName(team.getSongName());
@@ -272,6 +285,125 @@ public class TeamService {
             responseBody.setSuccess(false);
             responseBody.setError(responseError);
             return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(400));
+        }
+    }
+
+    //악보별 포지션 할당
+    public ResponseEntity<Response<ScorePositionDto>> scorePositionMapping(ScorePositionDto scorePositionDto){
+        Score score = scoreRepository.findByScoreId(scorePositionDto.getScoreId()).get();
+        score.setPositionId(positionRepository.findByPositionId(scorePositionDto.getPositionId()).get());
+        scoreRepository.save(score);
+        //응답
+        Response<ScorePositionDto> responseBody = new Response<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseBody.setSuccess(true);
+        responseBody.setData(scorePositionDto);
+        responseBody.setError(null);
+        return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(200));
+    }
+
+    //색상 조회
+    public ResponseEntity<Response<List<ColorDto>>> getColorList(){
+        List<ColorDto> colorData = new ArrayList<>();
+        List<Color> colorList = colorRepository.findAllByOrderByColorIdAsc();
+        for(Color color : colorList){
+            ColorDto colorDto = new ColorDto();
+            colorDto.setColorId(color.getColorId());
+            colorDto.setColorName(color.getColorName());
+            colorDto.setColorCode(color.getColorCode());
+            colorData.add(colorDto);
+        }
+
+        //응답
+        Response<List<ColorDto>> responseBody = new Response<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseBody.setSuccess(true);
+        responseBody.setData(colorData);
+        responseBody.setError(null);
+        return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(200));
+
+    }
+
+    //포지션 조회
+    public ResponseEntity<Response<List<PositionDto>>> getPositionList(Long id){
+        List<PositionDto> positionData = new ArrayList<>();
+        List<Position> positionList = positionRepository.findByTeam(teamRepository.findByTeamId(id).get());
+
+        if(positionList.isEmpty()){
+            List<Position> positions = new ArrayList<>();
+            for(int defaultIdx = 0; defaultIdx<7; defaultIdx++){
+                Position position = new Position();
+                position.setColor(colorRepository.findByColorId(DefaultColorId[defaultIdx]).get());
+                position.setPositionName(DefaultPositionName[defaultIdx]);
+                position.setTeam(teamRepository.findByTeamId(id).get());
+                positions.add(position);
+                System.out.println(123);
+            }
+            System.out.println(222);
+            positionRepository.saveAll(positions);
+            System.out.println(333);
+            positionList = positionRepository.findByTeam(teamRepository.findByTeamId(id).get());
+        }
+
+        for(Position position : positionList){
+            PositionDto positionDto = new PositionDto();
+            positionDto.setPositionId(position.getPositionId());
+            positionDto.setPositionName(position.getPositionName());
+            positionDto.setColorId(position.getColor().getColorId());
+            positionData.add(positionDto);
+        }
+
+        //응답
+        Response<List<PositionDto>> responseBody = new Response<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseBody.setSuccess(true);
+        responseBody.setData(positionData);
+        responseBody.setError(null);
+        return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(200));
+
+    }
+
+    //커스텀 포지션 생성
+    public ResponseEntity<Response<CustomPositionDto>> addCustomPosition(CustomPositionDto customPositionDto){
+        List<Position> positionList = positionRepository.findByTeam(teamRepository.findByTeamId(customPositionDto.getTeamId()).get());
+        String newPositionName = customPositionDto.getPositionName();
+        boolean isDuplicate = false;
+        for(int defaultIdx = 0; defaultIdx<7; defaultIdx++){
+            if(newPositionName.equals(DefaultPositionName[defaultIdx])){
+                isDuplicate = true;
+                break;
+            }
+        }
+        for(Position position : positionList){
+            if(newPositionName.equals(position.getPositionName())){
+                isDuplicate = true;
+                break;
+            }
+        }
+        if(isDuplicate){
+            //에러
+            Response<CustomPositionDto> responseBody = new Response<>();
+            HttpHeaders responseHeaders = new HttpHeaders();
+            ErrorResponse responseError = new ErrorResponse("400", "중복된 이름입니다.");
+            responseBody.setSuccess(false);
+            responseBody.setError(responseError);
+            return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(400));
+        }
+        else{
+            //등록
+            Position newPosition = new Position();
+            newPosition.setPositionName(customPositionDto.getPositionName());
+            newPosition.setTeam(teamRepository.findByTeamId(customPositionDto.getTeamId()).get());
+            newPosition.setColor(colorRepository.findByColorId(customPositionDto.getColorId()).get());
+            positionRepository.save(newPosition);
+
+            //응답
+            Response<CustomPositionDto> responseBody = new Response<>();
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseBody.setSuccess(true);
+            responseBody.setData(customPositionDto);
+            responseBody.setError(null);
+            return new ResponseEntity<>(responseBody,responseHeaders,HttpStatus.valueOf(200));
         }
     }
 
