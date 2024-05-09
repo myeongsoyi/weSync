c685be2a7f92492ee6bc82a1aa5c15e59c538f51
황인규 <rlawldbs42@gmail.com>
2024-05-10 08:56:00 +0900

    style: 테스트 컴포넌트로 일부 옮김

--- frontend/services/kakaoLogin.ts
+++ frontend/services/kakaoLogin.ts
@@ -1,6 +1,6 @@
 export const kakaoLoginAPI = async (code: string) => {
   const baseURL = process.env.NEXT_PUBLIC_API_URL;
-  const appURL = process.env.NEXT_PUBLIC_APP_URL;
+  // const appURL = process.env.NEXT_PUBLIC_APP_URL;
 
   try {
     const res = await fetch(`${baseURL}/users?code=${code}`);
