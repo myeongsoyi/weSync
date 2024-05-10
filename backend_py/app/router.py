bc759c8a5d6dc603954e1d18e03a782239b36fab
명소이 <myeongsoyi@gmail.com>
2024-05-10 13:46:11 +0900

    feat: pdf 형식 악보 업로드 response

--- backend_py/app/router.py
+++ backend_py/app/router.py
@@ -1,25 +1,35 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+from app.response import BaseResponse
+from app.response import CommonResponse
 
 rScore = APIRouter(prefix="/py-api/score")
 
-@rScore.post('/', tags = ['score'])
-async def upload_score(file: bytes = File()):
-    # pdf 파일 받기
-
-    # 받았다는 메시지 전송
-
+@rScore.post('/', tags = ['score'], response_model=BaseResponse)
+def upload_score(file: UploadFile = File(...)):
+    if(file == None): # 파일을 전송받지 못한 경우
+        return CommonResponse(False, None, 400, "파일 업로드에 실패했습니다. 다시 시도해주세요.")
+    if(file.content_type != "application/pdf"): # 지원하지 않는 파일 형식
+        return CommonResponse(False, None, 400, f"{file.content_type.split('/')[1]} 파일 형식을 지원하지 않습니다.")
     # 악보 인식
 
     # 악보 이미지 s3 저장
 
     # 반주 s3 저장
     
-    pass
+    return CommonResponse(
+        True,
+        {
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "messgae": f"{file.content_type.split('/')[1]} 형식의 파일이 업로드 되었습니다."
+        },
+        200, "파일 전송 성공!"
+        )
 
-@rScore.get('/{team_id}', tags = ['score'])
+@rScore.get('/{team_id}', tags = ['score'], response_model=BaseResponse)
 def get_scores():
     pass
 
-@rScore.delete('/{team_id}', tags = ['score'])
+@rScore.delete('/{team_id}', tags = ['score'], response_model=BaseResponse)
 def delete_scores():
     pass
