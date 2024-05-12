97f99ad8b312af918dc7a4590273ab08dfb72605
이병수 <lbsoo1021@naver.com>
2024-05-12 19:49:18 +0900

    fix: Add check for teamLeader 팀 리더가 맞는지 확인하는 로직추가

--- backend/src/main/java/com/ssafy/weSync/global/ApiResponse/CustomError.java
+++ backend/src/main/java/com/ssafy/weSync/global/ApiResponse/CustomError.java
@@ -14,7 +14,8 @@ public enum CustomError {
     NO_TEAMUSER(400, "존재하지 않는 팀원입니다."),
 
     // Notice
-    NO_NOTICE(400, "존재하지 않는 공지입니다.")
+    NO_NOTICE(400, "존재하지 않는 공지입니다."),
+    NO_TEAM_LEADER(401, "팀장이 아닙니다.")
     ;
 
     private final int errorCode;
