fb160b39ac355bd76574a684a09d4faa49053f5c
황인규 <rlawldbs42@gmail.com>
2024-05-12 21:04:56 +0900

    feat: 팀 목록 api 추가

--- frontend/components/common/FixedAudioPlayer/index.tsx
+++ frontend/components/common/FixedAudioPlayer/index.tsx
@@ -34,7 +34,17 @@ export default function FixedAudioPlayer() {
   }, [playing]); // playing이 변경될 때만 재생 상태 업데이트
 
   return (
-    <>
+    <div
+    style={{
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      height: '14vh',
+      minHeight: '100px',
+      backgroundColor: 'white',
+      zIndex: 1000,
+    }}>
       {isPlaying === false && (
         <div
           style={{
@@ -42,7 +52,7 @@ export default function FixedAudioPlayer() {
             bottom: 0,
             left: 0,
             width: '100%',
-            height: '14vh',
+            height: 'fit-content',
             minHeight: '100px',
           }}
         >
@@ -59,6 +69,6 @@ export default function FixedAudioPlayer() {
           />
         </div>
       )}
-    </>
+    </div>
   );
 }
