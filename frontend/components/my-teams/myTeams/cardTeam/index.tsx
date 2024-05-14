6c72c566ed4d86bea4810ba3732953714d06a6fc
yeon <lsy9752@naver.com>
2024-05-14 11:45:02 +0900

    design: UIUIX fix

--- frontend/components/my-teams/myTeams/cardTeam/index.tsx
+++ frontend/components/my-teams/myTeams/cardTeam/index.tsx
@@ -4,9 +4,7 @@ import { useState, useEffect } from 'react';
 import styles from './index.module.scss';
 import { useRouter } from 'next/navigation';
 import TeamCreate from '../../../team/information/teaminfomodal/createmodal';
-// import { getMainTeams } from '@/services/home/mainTeams';
 import { CrownFilled, PlusCircleOutlined } from '@ant-design/icons';
-// import styles from './index.module.scss';
 import { Avatar, Badge, Card, Tag, Tooltip } from 'antd';
 import { Group } from 'antd/es/avatar';
 import Image from 'next/image';
@@ -15,80 +13,71 @@ import { MyTotalTeams } from '@/types/myTeams';
 import { getMyTeams } from '@/services/my-teams';
 
 function useViewportWidth() {
-  const [width, setWidth] = useState(0); // 초기 너비 설정
+  const [width, setWidth] = useState(0);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // 초기 너비 설정 2
       setWidth(window.innerWidth);
 
       const handleResize = () => {
-        setWidth(window.innerWidth); // 창 크기가 변경될 때 너비 업데이트
+        setWidth(window.innerWidth);
       };
 
-      window.addEventListener('resize', handleResize); // 창 크기 변경 이벤트 리스너 추가
+      window.addEventListener('resize', handleResize);
 
       return () => {
-        window.removeEventListener('resize', handleResize); // 컴포넌트 언마운트 시 이벤트 리스너 제거
+        window.removeEventListener('resize', handleResize);
       };
     }
-  }, []); // 의존성 배열을 비워서 컴포넌트 마운트 시에만 실행
+  }, []);
 
   return width;
 }
 
 export default function CardTeams() {
-  const [memNum, setMemNum] = useState(5); // 너비에 따른 멤버 수 상태 변수
-  const [success, setSuccess] = useState<MyTotalTeams['success']>(true); // 성공 상태 변수
-  const [data, setData] = useState<MyTotalTeams['data']>([]); // 데이터 상태 변수
-  const [error, setError] = useState<MyTotalTeams['error']>(null); // 에러 상태 변수
-  const width = useViewportWidth(); // 너비 상태 변수
-  //   const teams = await getMainTeams();
+  const [memNum, setMemNum] = useState(5);
+  const [success, setSuccess] = useState<MyTotalTeams['success']>(true);
+  const [data, setData] = useState<MyTotalTeams['data']>([]);
+  const [error, setError] = useState<MyTotalTeams['error']>(null);
+  const width = useViewportWidth();
 
   useEffect(() => {
     const fetchMainTeams = async () => {
       const teams = await getMyTeams();
-      setSuccess(teams.success); // 성공 상태 변수 업데이트
-      setData(teams.data); // 데이터 상태 변수 업데이트
-      setError(teams.error); // 에러 상태 변수 업데이트
+      setSuccess(teams.success);
+      setData(teams.data);
+      setError(teams.error);
     };
     fetchMainTeams();
   }, []);
 
   useEffect(() => {
     if (width < 920) {
-      // 너비가 920px 미만일 때
-      setMemNum(4); // 멤버 수를 4명으로 설정
-      // } else if (width < 840) {
-      //   // 너비가 840px 미만일 때
-      //   setMemNum(4); // 멤버 수를 4명으로 설정
+      setMemNum(4);
     } else {
-      // 일반 데스크탑 화면일 때
-      setMemNum(5); // 멤버 수를 5명으로 설정
+      setMemNum(5);
     }
-  }, [width]); // 너비가 변경될 때마다 실행
+  }, [width]);
 
   const router = useRouter();
   console.log(router);
+
   if (!success) {
-    // api 요청 실패 시
     return (
       <div>
         <p>{error?.errorMessage}</p>
       </div>
     );
   }
+
   return (
     <>
       <div className="grid grid-cols-3 gap-4 mt-4">
         <Card
           style={{
-            // width: '32%',
-            // marginTop: 16,
             textAlign: 'center',
             borderRadius: '10px',
             border: '3px solid #FFC500',
-            // marginTop: '16px',
             minHeight: '230px',
           }}
           className="flex justify-center items-center cursor-pointer"
@@ -104,11 +93,7 @@ export default function CardTeams() {
         </Card>
         {data?.map((team, i) => (
           <div key={i} className={`${styles.cardContainer}`}>
-            <Link
-              href={`/team/${team.teamId}/information`}
-              // style={{ width: '32%' }}
-              // className="even:bg-amber-100 odd:bg-amber-50"
-            >
+            <Link href={`/team/${team.teamId}/information`}>
               {team.isFinished && (
                 <div className={styles.isEndOverlay}>
                   <h1>완료</h1>
@@ -117,8 +102,6 @@ export default function CardTeams() {
               )}
               <Card
                 style={{
-                  //   width: '32%',
-                  // marginTop: 16,
                   height: '100%',
                   textAlign: 'center',
                   borderRadius: '10px',
@@ -142,24 +125,24 @@ export default function CardTeams() {
                   </p>
                 </div>
                 {team.myPosition && (
-                <p>
-                  <Tag
-                    style={{
-                      border: `1px solid #${team.positionCode}`,
-                      color: `#${team.positionCode}`,
-                      margin: '0.75rem 0',
-                    }}
-                    bordered={false}
-                  >
-                    {team.myPosition}
-                  </Tag>
-                </p>
+                  <p>
+                    <Tag
+                      style={{
+                        border: `1px solid #${team.positionCode}`,
+                        color: `#${team.positionCode}`,
+                        margin: '0.75rem 0',
+                      }}
+                      bordered={false}
+                    >
+                      {team.myPosition}
+                    </Tag>
+                  </p>
                 )}
                 <Avatar
                   src={team.teamProfileUrl}
                   alt="팀"
                   size={80}
-                  style={{ borderColor: '#FFC500', marginBottom: '0.5rem'}}
+                  style={{ border: '3px solid #FFC500', marginBottom: '0.5rem' }}
                 />
                 <div className="flex mt-4 justify-center gap-1">
                   <Group>
@@ -167,9 +150,7 @@ export default function CardTeams() {
                       <Badge
                         count={
                           member.leader ? (
-                            <CrownFilled
-                              style={{ color: 'orange', fontSize: '24px' }}
-                            />
+                            <CrownFilled style={{ color: 'orange', fontSize: '24px' }} />
                           ) : (
                             0
                           )
@@ -177,22 +158,18 @@ export default function CardTeams() {
                         offset={[-18, -5]}
                         key={i}
                       >
-                        <Tooltip
-                          placement="top"
-                          title={member.nickName}
-                          arrow={true}
-                        >
+                        <Tooltip placement="top" title={member.nickName} arrow={true}>
                           <Avatar
                             src={member.userProfileUrl}
                             alt={member.nickName}
                             size={36}
-                            style={{ borderColor: '#FFC500' }}
+                            style={{ border: '2px solid #FFE27F' }}
                           ></Avatar>
                         </Tooltip>
                       </Badge>
                     ))}
                     {team.member.length > memNum && (
-                      <Avatar size={36} style={{ borderColor: '#FFC500' }}>
+                      <Avatar size={36} style={{ border: '3px solid #FFC500' }}>
                         +{team.member.length - memNum}
                       </Avatar>
                     )}
