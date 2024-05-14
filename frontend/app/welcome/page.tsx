2d749b47239cfe423d71ee3aa163e6dc283227fd
yeon <lsy9752@naver.com>
2024-05-14 17:59:58 +0900

    design: welcome and mainNav

--- frontend/app/welcome/page.tsx
+++ frontend/app/welcome/page.tsx
@@ -8,20 +8,16 @@ import Link from 'next/link';
 const contentStyle: React.CSSProperties = {
   height: '400px',
   color: '#fff',
-  lineHeight: '900px',
+  lineHeight: '400px',
   textAlign: 'center',
   background: '#364d79',
 };
 
-export default async function welcome() {
+export default function welcome() {
   return (
     <div>
-      <div className="welcome">
-        <div
-          style={{
-            padding: 12,
-          }}
-        >
+      <div className={styles.welcome}>
+        <div className={styles.content}>
           <div>
             <div className="flex justify-center">
               <Link href="/" className={styles.logo}>
@@ -37,7 +33,7 @@ export default async function welcome() {
             <div className={styles.kakao}>
               <LoginComponent />
             </div>
-            <div>
+            <div className={styles.carouselWrapper}>
               <Carousel autoplay autoplaySpeed={3000} speed={500}>
                 <div>
                   <h3 style={contentStyle}>1</h3>
@@ -49,6 +45,17 @@ export default async function welcome() {
                   <h3 style={contentStyle}>3</h3>
                 </div>
               </Carousel>
+              <div className={styles.floatingButton}>
+                <Link href="/">
+                  <Image
+                    src={'/svgs/start.svg'}
+                    alt="시작하기"
+                    width={200} // 크기 키움
+                    height={200} // 크기 키움
+                    className={styles.startButton} // 스타일 클래스 추가
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
--- frontend/components/home/mainTeams/index.tsx
+++ frontend/components/home/mainTeams/index.tsx
@@ -4,16 +4,18 @@ import Link from 'next/link';
 import { PlusOutlined } from '@ant-design/icons';
 
 export default async function MainTeams() {
-
   return (
     <div className={styles.outer}>
       <div className="flex gap-1 my-1">
-        <Link className='flex' href="/my-teams">
-          <h1 className="font-extrabold pl-3 ">TEAMS</h1>
-        <PlusOutlined style={{ fontSize: '20px', fontWeight: 500, paddingLeft: '6px'}} />
+        <Link className={styles.teamsLink} href="/my-teams">
+          <h1 className="font-extrabold pl-3">TEAMS</h1>
+          <PlusOutlined
+            className={styles.plusIcon}
+            style={{ fontSize: '25px', fontWeight: 'bold', marginLeft: '6px' }}
+          />
         </Link>
       </div>
-      <h2 className='pl-3 text-orange-400'>ON GOING</h2>
+      <h2 className="pl-3 text-orange-400">ON GOING</h2>
       <CardTeams />
     </div>
   );
