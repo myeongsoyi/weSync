b18cc25a56711bae3746a0db9df090999b4a5eff
yeon <lsy9752@naver.com>
2024-05-14 11:45:02 +0900

    design: UIUIX fix

--- frontend/components/common/navigations/navTeam.tsx
+++ frontend/components/common/navigations/navTeam.tsx
@@ -2,10 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { Layout, Button, Avatar, Dropdown, message, Popover } from 'antd';
-import {
-  SettingFilled,
-  CaretDownOutlined,
-} from '@ant-design/icons';
+import { SettingFilled, CaretDownOutlined } from '@ant-design/icons';
 import Swal from 'sweetalert2';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -13,42 +10,43 @@ import TeamModify from '@/components/team/information/teaminfomodal/modifymodal'
 import type { MenuProps } from 'antd';
 import styles from '@/components/team/information/members/memberList/index.module.scss';
 import LoginComponent from '@/components/common/login';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { getTeamInviteLink, getTeamDetail, deleteLeaveTeam, deleteRemoveTeam } from '@/services/team';
 import { TeamDetail } from '@/types/teamDetail';
 
 export default function TeamPage() {
-  const [success, setSuccess] = useState<TeamDetail['success']>(true); // 성공 상태 변수
+  const [success, setSuccess] = useState<TeamDetail['success']>(true);
   const [teamDetail, setTeamDetail] = useState<TeamDetail['data']>({} as TeamDetail['data']);
-  const [error, setError] = useState<TeamDetail['error']>(null); // 에러 상태 변수
-  const [isLeader, setIsLeader] = useState(false); // 팀장 여부
+  const [error, setError] = useState<TeamDetail['error']>(null);
+  const [isLeader, setIsLeader] = useState(false);
   const pathname = usePathname();
   const teamId = pathname.split('/')[2];
   const { Header } = Layout;
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const fetchTeamDetail = async (teamId: string) => {
+    const response = await getTeamDetail(teamId);
+    if (response.success) {
+      setSuccess(response.success);
+      setTeamDetail(response.data);
+      setIsLeader(response.data.teamLeader);
+    } else {
+      setSuccess(response.success);
+      setError(response.error);
+    }
+  };
 
   useEffect(() => {
-    const fetchTeamDetail = async () => {
-      const response = await getTeamDetail(teamId.toString());
-      if (response.success) {
-        setSuccess(response.success);
-        setTeamDetail(response.data);
-        setIsLeader(response.data.teamLeader);
-      } else {
-        setSuccess(response.success);
-        setError(response.error);
-      }
-      console.log(response);
-    };
-    fetchTeamDetail();
-  }, []);
+    fetchTeamDetail(teamId);
+  }, [pathname]);
 
   const handleOpenChange = (newOpen: boolean) => {
     setOpen(newOpen);
   };
-  async function handleMenuClick(key: string) {
+
+  const handleMenuClick = async (key: string) => {
     if (key === 'leave-team') {
-      // 팀 떠나기에 대한 간단한 확인
       Swal.fire({
         title: '정말로 팀을 떠나시겠습니까?',
         icon: 'warning',
@@ -62,14 +60,17 @@ export default function TeamPage() {
         if (result.isConfirmed) {
           const response = await deleteLeaveTeam(teamId);
           if (response.success) {
-            message.success('팀에서 나왔습니다.');
+            message.success({
+              content: '팀에서 나왔습니다.',
+              duration: 2, // 메시지가 2초 후에 사라짐
+            });
+            router.push('/');
           } else {
             message.error(response.error.errorMessage);
           }
         }
       });
     } else if (key === 'create-invite-link') {
-      // 초대 링크 생성
       const response = await getTeamInviteLink(teamId);
       if (response.success) {
         Swal.fire({
@@ -84,14 +85,10 @@ ${response.data.url}</textarea>
           confirmButtonText: '복사하기',
           cancelButtonText: '닫기',
           preConfirm: () => {
-            // 입력 필드의 내용을 선택하고 클립보드에 복사
-            const copyText = document.getElementById(
-              'inviteLink',
-            ) as HTMLTextAreaElement;
+            const copyText = document.getElementById('inviteLink') as HTMLTextAreaElement;
             copyText.select();
             document.execCommand('copy');
 
-            // 성공 메시지를 표시
             Swal.fire({
               title: '복사 완료!',
               text: '초대 링크가 클립보드에 복사되었습니다.',
@@ -110,25 +107,15 @@ ${response.data.url}</textarea>
         });
       }
     } else if (key === 'edit-team-info') {
-      // const currentTeam = teamDetail?.activeTeams.find((t) => t.teamId.toString() === teamId);
       if (teamDetail) {
-        TeamModify(
-          teamId,
-          teamDetail.teamName,
-          teamDetail.songName,
-          teamDetail.teamProfileUrl,
-          teamDetail.finished,
-        );
+        TeamModify(teamId, teamDetail.teamName, teamDetail.songName, teamDetail.teamProfileUrl, teamDetail.finished);
       }
     } else if (key === 'delete-team') {
-      // 팀 삭제하기에 대한 보다 엄격한 확인
       Swal.fire({
         title: '정말로 팀을 삭제하시겠습니까?',
         icon: 'warning',
         iconColor: '#d33',
-        customClass: {
-          popup: styles.borderRed,
-        },
+        customClass: { popup: styles.borderRed },
         input: 'text',
         inputLabel: '삭제된 정보는 복구되지 않습니다.',
         inputPlaceholder: '삭제하겠습니다.',
@@ -150,21 +137,23 @@ ${response.data.url}</textarea>
         if (result.isConfirmed) {
           const response = await deleteRemoveTeam(teamId);
           if (response.success) {
-            message.success('팀이 삭제되었습니다.');
+            message.success({
+              content: '팀이 삭제되었습니다.',
+              duration: 2, // 메시지가 2초 후에 사라짐
+            });
+            router.push('/');
           } else {
             message.error(response.error.errorMessage);
           }
         }
       });
     }
-  }
+  };
+
   const items: MenuProps['items'] = [
     {
       label: (
-        <p
-          style={{ fontWeight: 'bold', textAlign: 'center' }}
-          onClick={() => handleMenuClick('create-invite-link')}
-        >
+        <p style={{ fontWeight: 'bold', textAlign: 'center', cursor: 'pointer' }} onClick={() => handleMenuClick('create-invite-link')}>
           초대 링크 생성
         </p>
       ),
@@ -172,10 +161,7 @@ ${response.data.url}</textarea>
     },
     {
       label: (
-        <p
-          style={{ fontWeight: 'bold', textAlign: 'center' }}
-          onClick={() => handleMenuClick('set-position')}
-        >
+        <p style={{ fontWeight: 'bold', textAlign: 'center', cursor: 'pointer' }} onClick={() => handleMenuClick('set-position')}>
           내 포지션 설정
         </p>
       ),
@@ -183,61 +169,53 @@ ${response.data.url}</textarea>
     },
   ];
 
-  // 팀장일 때만 보이는 메뉴 아이템 추가
-if (isLeader) {
-  items.push(
-    {
-      label: (
-        <p
-          style={{ fontWeight: 'bold', textAlign: 'center', color: 'blue' }}
-          onClick={() => handleMenuClick('edit-team-info')}
-        >
-          팀 정보 변경
-        </p>
-      ),
-      key: 'edit-team-info',
-    },
-    {
-      label: (
-        <p
-          style={{ fontWeight: 'bold', color: 'red', textAlign: 'center' }}
-          onClick={() => handleMenuClick('leave-team')}
-        >
-          팀 나가기
-        </p>
-      ),
-      key: 'leave-team',
-    },
-    {
-      label: (
-        <p
-          style={{
-            fontWeight: 'bold',
-            textAlign: 'center',
-          }}
-          onClick={() => handleMenuClick('delete-team')}
-        >
-          팀 삭제
-        </p>
-      ),
-      key: 'delete-team',
-      style: { backgroundColor: '#d33', color: 'white' },
-    },
-  );
-}
+  if (isLeader) {
+    items.push(
+      {
+        label: (
+          <p style={{ fontWeight: 'bold', textAlign: 'center', color: 'blue', cursor: 'pointer' }} onClick={() => handleMenuClick('edit-team-info')}>
+            팀 정보 변경
+          </p>
+        ),
+        key: 'edit-team-info',
+      },
+      {
+        label: (
+          <p style={{ fontWeight: 'bold', color: 'red', textAlign: 'center', cursor: 'pointer' }} onClick={() => handleMenuClick('leave-team')}>
+            팀 나가기
+          </p>
+        ),
+        key: 'leave-team',
+      },
+      {
+        label: (
+          <p style={{ fontWeight: 'bold', textAlign: 'center', cursor: 'pointer' }} onClick={() => handleMenuClick('delete-team')}>
+            팀 삭제
+          </p>
+        ),
+        key: 'delete-team',
+        style: { backgroundColor: '#d33', color: 'white' },
+      },
+    );
+  }
 
   const content = (
     <div>
       {teamDetail?.activeTeams?.map((team, index) => (
         <div key={index} className="flex mt-1 first:mt-0">
-          <Link className="m-auto" href={`/team/${team.teamId}/information`}>
-            <div className="flex-col min-w-32">
-              <p className="text-center  text-gray-800  text-sm font-semibold">
-                {team.teamName}
-              </p>
-              <p className="text-center text-gray-400 text-xs">{team.songName}</p>
-            </div>
-          </Link>
+          <div className="flex-col min-w-32">
+            <p
+              className="text-center text-gray-800 text-sm font-semibold"
+              style={{ cursor: 'pointer' }}
+              onClick={() => {
+                router.push(`/team/${team.teamId}/information`);
+                setOpen(false); // 드롭다운 닫기
+              }}
+            >
+              {team.teamName}
+            </p>
+            <p className="text-center text-gray-400 text-xs">{team.songName}</p>
+          </div>
         </div>
       ))}
     </div>
@@ -250,7 +228,6 @@ if (isLeader) {
   return (
     <Layout style={{ backgroundColor: '#FFFFFF' }}>
       <Header
-        // className="flex"
         style={{
           display: 'flex',
           alignItems: 'center',
@@ -262,7 +239,12 @@ if (isLeader) {
       >
         <div
           className="flex-1"
-          style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '100%',
+            minWidth: '170px',
+          }}
         >
           <Link href="/" passHref>
             <span
@@ -272,13 +254,7 @@ if (isLeader) {
                 alignItems: 'center',
               }}
             >
-              <Image
-                src={'/svgs/logo.svg'}
-                alt="로고"
-                width={170}
-                height={100}
-                className="h-auto"
-              />
+              <Image src={'/svgs/logo.svg'} alt="로고" width={170} height={100} className="h-auto" />
             </span>
           </Link>
         </div>
@@ -290,30 +266,25 @@ if (isLeader) {
               <Avatar
                 size={55}
                 src={teamDetail?.teamProfileUrl}
-                style={{ marginRight: '10px' }}
+                style={{
+                  marginRight: '10px',
+                  border: '3px solid #FFC500',
+                }}
               />{' '}
               {/* 간격 추가 */}
             </div>
-            <div className="flex-row">
-              <p className="text-center text-4xl text-gray-700 font-bold w-40">
-                {teamDetail?.teamName ?? '팀 이름'}
-              </p>
-              <p className="text-center text-sm text-gray-500 font-bold w-40">
+            <div className="flex flex-col items-center">
+              <Link href={`/team/${teamId}/information`}>
+                <p className="text-4xl text-gray-700 font-bold" style={{ whiteSpace: 'nowrap', cursor: 'pointer' }}>
+                  {teamDetail?.teamName ?? '팀 이름'}
+                </p>
+              </Link>
+              <p className="text-sm text-gray-500 font-bold" style={{ whiteSpace: 'nowrap' }}>
                 {teamDetail?.songName ?? '미정'}
               </p>
 
-              <div
-                className="flex justify-center w-40"
-                style={{ whiteSpace: 'nowrap' }}
-              >
-                <Popover
-                  content={content}
-                  arrow={false}
-                  trigger="click"
-                  placement="bottom"
-                  open={open}
-                  onOpenChange={handleOpenChange}
-                >
+              <div className="flex justify-center" style={{ whiteSpace: 'nowrap' }}>
+                <Popover content={content} arrow={false} trigger="click" placement="bottom" open={open} onOpenChange={handleOpenChange}>
                   <Button
                     type="text"
                     style={{
@@ -333,10 +304,7 @@ if (isLeader) {
             </div>
           </div>
           <Dropdown menu={{ items }} trigger={['click']}>
-            <Button
-              type="text"
-              style={{ padding: '0 10px 5px', margin: 'auto 0' }}
-            >
+            <Button type="text" style={{ padding: '0 10px 5px', margin: 'auto 0' }}>
               <SettingFilled style={{ color: 'gray', fontSize: '20px' }} />
             </Button>
           </Dropdown>
