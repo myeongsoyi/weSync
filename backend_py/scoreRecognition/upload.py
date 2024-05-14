1a65af411478b0dbbac451fbc2d7ab7fe7a98010
명소이 <myeongsoyi@gmail.com>
2024-05-14 15:29:34 +0900

    feat: 주소 수정 및 upload 주석처리
    Former-commit-id: 8eae601961fb792128ac2d9b33fa3066b8b7ea8c

--- backend_py/scoreRecognition/upload.py
+++ backend_py/scoreRecognition/upload.py
@@ -1,17 +1,17 @@
-import boto3
-import os
+# import boto3
+# import os
 
-def upload_file_to_s3(file_name, object_name=None):
-    # S3 서비스에 대한 클라이언트 생성
-    s3 = boto3.client('s3', aws_access_key_id=os.getenv("cloud_aws_credentials_access-key"),
-                      aws_secret_access_key=os.getenv("cloud_aws_credentials_secret-key"),
-                      region_name=os.getenv("cloud_aws_region_static"))
+# def upload_file_to_s3(file_name, object_name=None):
+#     # S3 서비스에 대한 클라이언트 생성
+#     s3 = boto3.client('s3', aws_access_key_id=os.getenv("cloud_aws_credentials_access-key"),
+#                       aws_secret_access_key=os.getenv("cloud_aws_credentials_secret-key"),
+#                       region_name=os.getenv("cloud_aws_region_static"))
 
-    # object_name이 지정되지 않은 경우, 로컬 파일 이름을 사용
-    if object_name is None:
-        object_name = file_name
+#     # object_name이 지정되지 않은 경우, 로컬 파일 이름을 사용
+#     if object_name is None:
+#         object_name = file_name
 
-    # 파일 업로드
-    s3.upload_file(file_name, os.getenv("cloud_aws_s3_bucket"), object_name)
+#     # 파일 업로드
+#     s3.upload_file(file_name, os.getenv("cloud_aws_s3_bucket"), object_name)
 
-    print(f"'{file_name}' has been uploaded to '{os.getenv("cloud_aws_s3_bucket")}' as '{object_name}'")
+#     print(f"'{file_name}' has been uploaded to '{os.getenv("cloud_aws_s3_bucket")}' as '{object_name}'")
