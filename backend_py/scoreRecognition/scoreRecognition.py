34ac1a16940f3d4b4825f231310611bcf51ff9e3
명소이 <myeongsoyi@gmail.com>
2024-05-15 21:16:46 +0900

    feat: error msg 변경

--- backend_py/scoreRecognition/scoreRecognition.py
+++ backend_py/scoreRecognition/scoreRecognition.py
@@ -151,7 +151,7 @@ def midi_to_ly(midi_path, ly_path):
 
 def ly_to_png(ly_path, output_base):
     try:
-        subprocess.run(['lilypond', '--png', '-o', output_base, ly_path], check=True)
+        subprocess.run(['lilypond', '--png', '-o', output_base, ly_path],  check=True)
         print(f"LilyPond to PNG conversion successful: {ly_path} -> {output_base}.png")
     except subprocess.CalledProcessError as e:
-        print(f"Error converting LilyPond to PNG: {e}")
+        print(f"Error converting LilyPond to PNG: {e}\nSTDERR: {e.stderr.decode()}")
