7bd85f308d2bbbc89fd140079e20a2da0de0c926
이병수 <lbsoo1021@naver.com>
2024-05-16 03:07:34 +0900

    ref: Update teamUser repo

--- backend/src/main/java/com/ssafy/weSync/feedback/repository/FeedBackRepository.java
+++ backend/src/main/java/com/ssafy/weSync/feedback/repository/FeedBackRepository.java
@@ -8,6 +8,4 @@ import org.springframework.data.repository.query.Param;
 
 public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
 
-    @Query("SELECT tu FROM TeamUser tu JOIN tu.team t JOIN tu.user u WHERE u.userId = :userId And t.teamId = :teamId")
-    TeamUser findByUserIdAndTeamId(@Param("userId") Long userId, @Param("teamId") Long teamId);
 }
