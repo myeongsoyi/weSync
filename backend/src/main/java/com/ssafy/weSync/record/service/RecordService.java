1fb039769e97dda11d56f32091ffb78868f2fab4
이병수 <lbsoo1021@naver.com>
2024-05-15 13:05:11 +0900

    feat: Add getTeamRecordList -ing

--- backend/src/main/java/com/ssafy/weSync/record/service/RecordService.java
+++ backend/src/main/java/com/ssafy/weSync/record/service/RecordService.java
@@ -5,19 +5,29 @@ import com.ssafy.weSync.global.ApiResponse.GlobalException;
 import com.ssafy.weSync.record.dto.request.CreateRequest;
 import com.ssafy.weSync.record.dto.response.GetAllMyResponse;
 import com.ssafy.weSync.record.dto.response.CreateResponse;
+import com.ssafy.weSync.record.dto.response.GetAllTeamCommon;
+import com.ssafy.weSync.record.dto.response.GetAllTeamResponse;
 import com.ssafy.weSync.record.entity.Record;
 import com.ssafy.weSync.record.repository.RecordRepository;
 import com.ssafy.weSync.s3.service.S3Service;
 import com.ssafy.weSync.team.entity.Score;
+import com.ssafy.weSync.team.entity.Team;
 import com.ssafy.weSync.team.entity.TeamUser;
 import com.ssafy.weSync.team.repository.ScoreRepository;
+import com.ssafy.weSync.team.repository.TeamRepository;
 import com.ssafy.weSync.team.repository.TeamUserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +42,7 @@ public class RecordService {
     private final RecordRepository recordRepository;
     private final ScoreRepository scoreRepository;
     private final TeamUserRepository teamUserRepository;
+    private final TeamRepository teamRepository;
 
     /***
      *
@@ -60,4 +71,34 @@ public class RecordService {
                 .collect(Collectors.toList());
         return getAllMyResponses;
     }
+
+    public List<GetAllTeamCommon> getTeamRecordList(Long teamId, String filter, Long userId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(() -> new GlobalException(CustomError.NO_TEAM));
+
+        List<GetAllTeamCommon> getAllTeamCommons;
+        if (filter.equals("all")){
+            getAllTeamCommons = getAllTeamRecordList(teamId);
+        }
+        if (filter.equals("my")){
+            return getAllMyTeamRecordList(teamId);
+        }
+        if (filter.substring(0, 3).equals("pos")){
+            return getAllTeamRecordListByPos(teamId, userId);
+        }
+
+    }
+
+
+    private List<GetAllTeamResponse> getAllTeamRecordList(Long teamId) {
+        List<Record> records = recordRepository.findAllByTeamId(teamId);
+        List<GetAllTeamResponse> getAllTeamResponses = records.stream()
+                .map(GetAllTeamResponse::toDto)
+                .collect(Collectors.toList());
+        return getAllTeamResponses;
+    }
+    private List<GetAllTeamCommon> getAllTeamRecordListByPos(Long teamId) {
+    }
+    private List<GetAllTeamCommon> getAllMyTeamRecordList(Long userId) {
+    }
+
 }
