25ec008ab4c437f45353a882b8205bf9ab90bc57
명소이 <myeongsoyi@gmail.com>
2024-05-15 13:38:53 +0900

    feat: s3 파일 업로드명 변경 및 await 삭제

--- backend_py/app/router.py
+++ backend_py/app/router.py
@@ -6,14 +6,14 @@ from scoreRecognition.scoreRecognition import recognition
 rScore = APIRouter(prefix="/py-api/score")
 
 @rScore.post('/', tags = ['score'], response_model=BaseResponse)
-async def upload_score(file: UploadFile = File(...)):
+def upload_score(file: UploadFile = File(...)):
     if(file == None): # 파일을 전송받지 못한 경우
         return CommonResponse(False, None, 400, "파일 업로드에 실패했습니다. 다시 시도해주세요.")
     
     if(file.content_type != "application/pdf"): # 지원하지 않는 파일 형식
         return CommonResponse(False, None, 400, f"{file.content_type.split('/')[1]} 파일 형식을 지원하지 않습니다.")
     
-    await recognition(file)
+    recognition(file)
     
     return CommonResponse(
         True,
