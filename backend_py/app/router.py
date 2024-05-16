ae43b577bc8e3061dd657afef61041580ddb35cb
명소이 <myeongsoyi@gmail.com>
2024-05-16 14:28:16 +0900

    feat: 악보 업로드 변수 수정 및 빈 악보 생성 db 추가
    Former-commit-id: 514d7d5b81ef0c761b0db269cf2a224236a4d81e

--- backend_py/app/router.py
+++ backend_py/app/router.py
@@ -18,15 +18,15 @@ def get_db():
 
 rScore = APIRouter(prefix="/py-api/score")
 
-@rScore.post('/', tags = ['score'], response_model=BaseResponse)
-def upload_score(file: UploadFile = File(...), db: Session = Depends(get_db)):
+@rScore.post('/{team_id}', tags = ['score'], response_model=BaseResponse)
+def upload_score(team_id: int, file: UploadFile = File(...), db: Session = Depends(get_db)):
     if(file == None): # 파일을 전송받지 못한 경우
         return CommonResponse(False, None, 400, "파일 업로드에 실패했습니다. 다시 시도해주세요.")
     
     if(file.content_type != "application/pdf"): # 지원하지 않는 파일 형식
         return CommonResponse(False, None, 400, f"{file.content_type.split('/')[1]} 파일 형식을 지원하지 않습니다.")
     
-    recognition(file, db)
+    recognition(file, team_id, db)
     
     return CommonResponse(
         True,
@@ -72,11 +72,14 @@ def delete_scores(team_id: int, db: Session = Depends(get_db)):
         200, "악보 삭제 성공!"
         )
 
-@rScore.post('/{team_id}', tags = ['score'], response_model=BaseResponse)
-def upload_score():
+@rScore.post('/{team_id}/empty', tags = ['score'], response_model=BaseResponse)
+def upload_score(team_id: int, db: Session = Depends(get_db)):
     co.createEmptyScore(f"{os.getcwd()}/scoreRecognition/Output/img/emptyScore.png")
     up.upload_file_to_s3(f"{os.getcwd()}/scoreRecognition/Output/img/emptyScore.png", "emptyScore.png")
-
+    db_img = Score(part_num = i, position_id = None, title = None, \
+                                score_url = f'https://{os.getenv("cloud_aws_s3_bucket")}.s3.{os.getenv("cloud_aws_region_static")}.amazonaws.com/{"scoreOutput/png/"}emptyScore.png', team_id = team_id )
+    db.add(db_img)
+    db.commit()
     return CommonResponse(
         True,
         {
