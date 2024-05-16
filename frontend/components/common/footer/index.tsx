c9419472907faec147b690cf8fab5cd5feb76ff9
yeon <lsy9752@naver.com>
2024-05-16 18:01:08 +0900

    design: info page

--- frontend/components/common/footer/index.tsx
+++ frontend/components/common/footer/index.tsx
@@ -1,56 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
-import { deleteUser } from '@/services/logout';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Footer() {
-  const [clickCount, setClickCount] = useState(0);
-  const [clickTimer, setClickTimer] = useState<number | undefined>(undefined);
-  const router = useRouter();
-  const clickThreshold = 7;
-  const clickTimeout = 800; // 클릭 간격 (밀리초)
-
-  useEffect(() => {
-    return () => {
-      if (clickTimer) {
-        clearTimeout(clickTimer); // 컴포넌트 언마운트 시 타이머 정리
-      }
-    };
-  }, []);
-
-  const handleIconClick = async () => {
-    clearTimeout(clickTimer);  // 기존 타이머를 클리어합니다.
-    setClickTimer(window.setTimeout(() => {
-      setClickCount(0); // 클릭 카운트 리셋
-    }, clickTimeout));
-    
-    console.log('Icon clicked', clickCount + 1, 'times.');
-    setClickCount((prevCount) => {
-      const updatedCount = prevCount + 1;
-      return updatedCount;
-    });
-
-    if (clickCount + 1 === clickThreshold) {
-      deleteUserRequest(); // deleteUser 요청 처리
-    }
-  };
-  
-  const deleteUserRequest = async () => {
-    if (clickCount + 1 === clickThreshold && window.confirm('정말로 회원 탈퇴를 진행하시겠습니까?')) {
-      deleteUser()
-        .then(() => {
-          router.push('/welcome');
-          console.log('User deleted successfully.');
-        })
-        .catch((error) => {
-          console.error('Failed to delete user:', error);
-        });
-    }
-  };
-
   return (
     <footer className={`${styles.footer} footer`}>
       <div className={`${styles.flex1} ${styles.justifyStart}`}>
@@ -69,11 +24,20 @@ export default function Footer() {
       </div>
       <div className={`${styles.flex1} ${styles.justifyCenter}`}>
         <p className={`m-auto w-fit ${styles.center}`}>
-          weSync {new Date().getFullYear()} Created by ©OneHarMony
+          weSync {new Date().getFullYear()} Created by OneHarMony™
         </p>
+        <div className={styles.links}>
+          <Link href="/welcome">INFO</Link>
+          <span className={styles.separator}>|</span>
+          <Link href="/">HOME</Link>
+          <span className={styles.separator}>|</span>
+          <span style={{ fontSize: '15px', color: 'lightgray' }}>
+            SERVICE : 멀티캠퍼스 역삼 (02-3429-5114)
+          </span>
+        </div>
       </div>
       <div className={`${styles.flex1} ${styles.justifyEnd}`}>
-        <div className={styles.icon} onClick={handleIconClick}>
+        <div className={styles.icon}>
           <Image
             src={'/svgs/wesync_icon.svg'}
             alt="아이콘"
