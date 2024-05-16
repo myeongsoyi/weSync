68d4fd36e7b7fa3d7adfcd5155639701c5fb0536
명소이 <myeongsoyi@gmail.com>
2024-05-16 17:05:14 +0900

    feat: 조회결과 row -> dict 변환
    Former-commit-id: b81ed402f5fb17a6489afa6272b2419064db9710

--- backend_py/app/router.py
+++ backend_py/app/router.py
@@ -50,7 +50,7 @@ def get_scores(team_id: int, db: Session = Depends(get_db)):
     
     return CommonResponse(
         True,
-        {"Data":scoreData},
+        {"Data": dict(scoreData)},
         200, "조회 성공!"
         )
 
