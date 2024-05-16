4e7ff329a45d09547261a2adf3013dd503e23b45
명소이 <myeongsoyi@gmail.com>
2024-05-16 16:59:24 +0900

    feat: position, color optional
    Former-commit-id: 6847e59c5d7289d0386c8a518cba074ac09d8b48

--- backend_py/app/schema.py
+++ backend_py/app/schema.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
-
-
+from typing import Optional
 
 class ScoreResponseDTO(BaseModel):
     score_url: str
     accompaniment_url: str
-    position_name: str
-    color_code: str
+    position_name: Optional[str] = None
+    color_code: Optional[str] = None
