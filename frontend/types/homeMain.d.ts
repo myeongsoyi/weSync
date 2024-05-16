fc6bf925fd532015f60cb25f00d1b3e20fa49929
황인규 <rlawldbs42@gmail.com>
2024-05-16 09:45:21 +0900

    fix: 메인화면 녹음 테이블 수정

--- frontend/types/homeMain.d.ts
+++ frontend/types/homeMain.d.ts
@@ -32,12 +32,15 @@ export interface MainActiveTeams {
 //   "recordId": number, // 녹음 id
 //   "title": string, // 녹음 파일명
 //   "recordUrl": string, // 녹음파일 주소
+//   "isPublic": boolean // 공개 비공개 여부
+//   "startAt": number // 녹음 시작시점(초:밀리초)
+//   "endAt": number // 녹음 종료시점 (초:밀리초)
+//   "createAt": string // 녹음 일시
 //   "teamId": number, // 녹음을 수행한 팀스페이스 id
 //   "teamUrl": string, // 팀 프로필 이미지 주소
 //   "songName": string // 노래 제목
 //   "positionName": string, // 녹음한 파트이름
-//   "startAt": number // 노래에서 녹음이 시작되는 시간(밀리초)
-//   "createAt": string // 녹음 일시
+//   "colorCode": string // 컬러코드
 //   },
 // ]
 
@@ -47,14 +50,15 @@ export interface MainRecords {
     recordId: number;
     title: string;
     recordUrl: string;
+    isPublic: boolean;
+    startAt: number;
+    createAt: string;
+    endAt: number;
     teamId: number;
     teamUrl: string;
     songName: string;
     positionName: string;
     colorCode: string;
-    startAt: number;
-    endAt: number;
-    createAt: string;
   }[];
   error: {
     errorCode: string;
