c3fb482bf509bf3f4dd24e248316162f63bba944
명소이 <myeongsoyi@gmail.com>
2024-05-17 18:26:59 +0900

    feat: base response data datatype  변경
    Former-commit-id: cd671b6f7d9ecdf80e19b31ec7302fa109cc384c

--- backend_py/app/response.py
+++ backend_py/app/response.py
@@ -7,10 +7,10 @@ class ErrorResponse(BaseModel):
 
 class BaseResponse(BaseModel):
     success: bool
-    data: Optional[dict] = None
+    data: Optional[Union[dict,list]] = None
     error: Optional[ErrorResponse] = None
 
-def CommonResponse(success: bool, data: Optional[Union[dict,list]]=None, errorCode: Optional[int]=None, errorMessage: Optional[str]=None):
+def CommonResponse(success: bool, data: Optional[dict]=None, errorCode: Optional[int]=None, errorMessage: Optional[str]=None):
     return BaseResponse(
         success=success,
         data=data,
