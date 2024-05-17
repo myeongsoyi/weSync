f08fa7807655a4f22bfdb4d63b42f5c7aaa110bd
황인규 <rlawldbs42@gmail.com>
2024-05-17 22:41:52 +0900

    feat: 악보에 해당하는 파트 출력

--- frontend/components/team/record/positions/index.tsx
+++ frontend/components/team/record/positions/index.tsx
@@ -8,13 +8,10 @@ interface IParams {
 }
 
 export default async function RecordPositions({ teamId }: IParams) {
-  const positions = await getTeamRecordPositions();
 
   return (
     <div className={styles.section}>
-      {positions.map((position: IRecord) => (
-        <PositionBox key={position.id} position={position} teamId={teamId} />
-      ))}
+        <PositionBox teamId={teamId} />
       <PositionAdd />
     </div>
   );
--- frontend/components/team/record/positions/positionBox/index.tsx
+++ frontend/components/team/record/positions/positionBox/index.tsx
@@ -1,92 +1,171 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
 import Image from 'next/image';
 import styles from './index.module.scss';
-import { EditOutlined } from '@ant-design/icons';
-import { Button, Slider, Tag } from 'antd';
-import { IRecord } from '@/services/team/record';
+import { CheckCircleOutlined, EditOutlined } from '@ant-design/icons';
+import { Button, Slider, Tag, Radio } from 'antd';
+import type { RadioChangeEvent } from 'antd';
+import { getScoreData } from '@/services/team/record';
+import { ScoreResponse } from '@/types/record';
+import { useRecordAudioStore } from '@/store/recordAudioStore';
+import { stat } from 'fs';
 
 interface IParams {
   teamId: string;
-  position: IRecord;
 }
 
-export default function PositionBox({ teamId, position }: IParams) {
-  const [isMute, setIsMute] = useState(false);
-  const [volume, setVolume] = useState(30);
-  const [savedVolume, setSavedVolume] = useState(30);
+interface IVolume {
+  index: number;
+  volume: number;
+  isMute: boolean;
+}
 
-  function changeVolume(value: number) {
-    setIsMute(false);
-    setVolume(value);
+export default function scoreBox({ teamId }: IParams) {
+  // const [isMute, setIsMute] = useState([]);
+  const [volume, setVolume] = useState<IVolume[]>([]);
+  const [success, setSuccess] = useState<ScoreResponse['success']>(false);
+  const [score, setScore] = useState<ScoreResponse['data']>([]);
+  const [error, setError] = useState<ScoreResponse['error']>(null);
+  const { scoreIndex, setScoreIndex } = useRecordAudioStore((state) => ({
+    scoreIndex: state.scoreIndex,
+    setScoreIndex: state.setScoreIndex,
+  }));
+
+  useEffect(() => {
+    const fetchScore = async () => {
+      const response = await getScoreData(teamId);
+      console.log(response);
+      if (response.success) {
+        setSuccess(response.success);
+        setScore(response.data);
+        setError(response.error);
+        setVolume(
+          response.data.map(
+            (_: ScoreResponse['data'][number], index: number) => ({
+              index,
+              volume: 30,
+              isMute: false,
+            }),
+          ),
+        );
+      } else {
+        setSuccess(response.success);
+        setError(response.error);
+      }
+    };
+    fetchScore();
+  }, []);
+
+  useEffect(() => {
+    console.log(scoreIndex);
+  }, [scoreIndex]);
+
+  function changeVolume(index: number, value: number) {
+    setVolume((prev) => {
+      const newVolume = [...prev];
+      newVolume[index].volume = value;
+      newVolume[index].isMute = false;
+      return newVolume;
+    });
   }
 
-  function clickMute() {
-    if (isMute) {
-      setVolume(savedVolume);
-    } else {
-      setSavedVolume(volume);
-      setVolume(0);
-    }
-    setIsMute(!isMute);
+  function clickMute(index: number) {
+    const isMute = volume[index].isMute;
+    setVolume((prev) => {
+      const newVolume = [...prev];
+      newVolume[index].isMute = !isMute;
+      return newVolume;
+    });
+  }
+
+  if (!success) {
+    return <p>{error?.errorMessage}</p>;
   }
 
   return (
-    <div className={styles.controller}>
-      <p className="hidden">{teamId}</p>
-      <Button
-        onClick={clickMute}
-        type="text"
-        style={{
-          padding: '1px',
-          width: '20%',
-          margin: 'auto',
-          height: 'auto',
-        }}
-      >
-        {!isMute ? (
-          <Image
-            src={'/images/volume_on.png'}
-            alt="볼륨온"
-            width={50}
-            height={50}
-          />
-        ) : (
-          <Image
-            src={'/images/volume_mute.png'}
-            alt="볼륨오프"
-            width={50}
-            height={50}
-          />
-        )}
-      </Button>
-      <div className={styles.box_slider}>
-        {position.name ? (
-          <Tag
-            style={{
-              color: `${position.color}`,
-              borderColor: `${position.color}`,
-              fontSize: 15,
-              width: '100%',
-              padding: 4,
-              textAlign: 'center',
-              marginTop: 10,
-            }}
-          >
-            {position.name}
-          </Tag>
-        ) : (
-          <p className="text-center">
-            <EditOutlined /> 포지션 할당
-          </p>
-        )}
-        <Slider
-          defaultValue={30}
-          value={volume}
-          onChange={changeVolume}
-        ></Slider>
-      </div>
+    <div className={styles.section}>
+      {score.map((score, index) => (
+        <div className={styles.controller} key={index}>
+          <div>
+            <Button
+              onClick={() => clickMute(index)}
+              type="text"
+              style={{
+                padding: '1px',
+                width: '100%',
+                margin: 'auto',
+                height: 'auto',
+                zIndex: 10,
+              }}
+            >
+              {!volume[index].isMute ? (
+                <Image
+                  src={'/images/volume_on.png'}
+                  alt="볼륨온"
+                  width={50}
+                  height={50}
+                  className="m-auto"
+                />
+              ) : (
+                <Image
+                  src={'/images/volume_mute.png'}
+                  alt="볼륨오프"
+                  width={50}
+                  height={50}
+                  className="m-auto"
+                />
+              )}
+            </Button>
+            <Button
+              onClick={() => setScoreIndex(index)}
+              type="text"
+              style={{
+                padding: '1px',
+                width: '100%',
+                margin: 'auto',
+                height: 'auto',
+                zIndex: 10,
+              }}
+            >
+              <CheckCircleOutlined
+                style={
+                  index === scoreIndex
+                    ? { color: 'blue', fontSize: 21 }
+                    : { color: 'darkgray', fontSize: 14 }
+                }
+                className="mb-2 mr-1"
+              />
+            </Button>
+          </div>
+          <div className={styles.box_slider}>
+            {score.position_name ? (
+              <Tag
+                style={{
+                  color: `${score.color_code}`,
+                  borderColor: `${score.color_code}`,
+                  fontSize: 15,
+                  width: '100%',
+                  padding: 4,
+                  textAlign: 'center',
+                  marginTop: 10,
+                }}
+              >
+                {score.position_name}
+              </Tag>
+            ) : (
+              <p className="text-center">
+                <EditOutlined /> 포지션 할당
+              </p>
+            )}
+            <Slider
+              defaultValue={30}
+              value={volume[index].isMute ? 0 : volume[index].volume}
+              onChange={(value) => changeVolume(index, value)}
+            ></Slider>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
